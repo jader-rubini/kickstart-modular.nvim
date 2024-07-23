9b9b3beb21a8233f45e5ac6a6646c5c672846ad3
Rubini&Co <140863793+rubiniandco@users.noreply.github.com>
2024-07-23 17:12:15 -0300

    config for undotree

--- lua/custom/plugins/undotree.lua
+++ lua/custom/plugins/undotree.lua
@@ -3,10 +3,12 @@
 
 return {
   'mbbill/undotree',
-  opts = {
-    undotree_SetFocusWhenToggle = 1,
-  },
   config = function()
+    -- Display short timestamps and auto-focus when open
+    vim.g.undotree_ShortIndicators = 1
+    vim.g.undotree_SetFocusWhenToggle = 1
+
+    -- Set custom keymap
     vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, { desc = 'Toggle undotree view' })
   end,
 }
