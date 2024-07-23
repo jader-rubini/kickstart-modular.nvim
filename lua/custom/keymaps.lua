7e0e3e022ac0f21f9fb72398c306ef89e9282b73
Rubini&Co <140863793+rubiniandco@users.noreply.github.com>
2024-07-23 09:40:33 -0300

    added a few awesome keymaps

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -2,3 +2,10 @@ print 'hello from custom keymaps'
 
 -- While in insert mode, typing `jj` will take you back to normal mode.
 vim.keymap.set('i', 'jj', '<Esc>')
+
+-- Map `U` to redo
+vim.keymap.set('n', 'U', '<C-r>', { noremap = true })
+
+-- Insert empty line ab[O]ve or bel[o]w and remain in normal mode.
+vim.keymap.set('n', '<leader>O', 'm`O<Esc>``', { desc = 'Add new empty line above' })
+vim.keymap.set('n', '<leader>o', 'm`o<Esc>``', { desc = 'Add new empty line below' })
